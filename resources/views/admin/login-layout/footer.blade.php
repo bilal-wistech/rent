7496de060bae894e4108fdba45045697ea0f4aab
bilalahmed1 <bilal.ahmed@wistech.biz>
2024-09-25 11:18:07 +0500

    initial commit

---
+++ resources/views/admin/login-layout/footer.blade.php
@@ -0,0 +1,9 @@
+</div>
+</div>
+<script src="{{ asset('js/jquery-2.2.4.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/js/admin-login.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.validate.min.js') }}"></script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</body>
+</html>
