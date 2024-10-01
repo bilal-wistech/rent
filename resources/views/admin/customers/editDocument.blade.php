a748edf4c3d49a858487b3d8f1d3c940742f8d09
maryam-malik86 <maryamalik0707@gmail.com>
2024-10-01 14:51:58 +0500

    alert box update

--- resources/views/admin/customers/editDocument.blade.php
+++ resources/views/admin/customers/editDocument.blade.php
@@ -9,8 +9,9 @@
             <div class="col-md-12">
                 <div class="box">
                     @if (session('success'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('success') }}
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                       {{session('success')  }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                       </div>
                     @endif
 
