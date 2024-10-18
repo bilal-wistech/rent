3a3e29796c6fb1c266654cca30bb33c9e2ec80f6
RanaSikander44 <sikander@wistech.biz>
2024-10-18 17:05:41 +0500

    today vacant properties

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,13 +14,21 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        
-        $data['total_users_count']        = User::count();
-        $data['total_property_count']     = Properties::count();
+
+
+        $bookedPropertyIds = DB::table('bookings')->pluck('property_id');
+        $unbookedPropertyIds = DB::table('properties')
+            ->whereNotIn('id', $bookedPropertyIds)
+            ->pluck('id');
+
+        dd($unbookedPropertyIds);
+
+        $data['total_users_count'] = User::count();
+        $data['total_property_count'] = Properties::count();
         $data['total_reservations_count'] = Bookings::count();
 
-        $data['today_users_count']        = User::whereDate('created_at', DB::raw('CURDATE()'))->count();
-        $data['today_property_count']     = Properties::whereDate('created_at', DB::raw('CURDATE()'))->count();
+        $data['today_users_count'] = User::whereDate('created_at', DB::raw('CURDATE()'))->count();
+        $data['today_property_count'] = Properties::whereDate('created_at', DB::raw('CURDATE()'))->count();
         $data['today_reservations_count'] = Bookings::whereDate('created_at', DB::raw('CURDATE()'))->count();
 
         $properties = new Properties;
