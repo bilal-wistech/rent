600c65f82eeadb757a24de19558187707024efcb
RanaSikander44 <sikander@wistech.biz>
2024-10-18 15:11:13 +0500

    new dashboard integrate

--- resources/views/admin/common/head.blade.php
+++ resources/views/admin/common/head.blade.php
@@ -1,55 +1,34 @@
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html lang="en">
+<!--begin::Head-->
 
-			<title> {{ siteName() }} | Dashboard </title>
-
-			<meta name="csrf-token" content="{{ csrf_token() }}">
-
-			<!-- Tell the browser to be responsive to screen width -->
-			<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-			<!-- Bootstrap v5.3.2 -->
-			<link rel="stylesheet" href="{{ asset('backend/bootstrap/css/bootstrap.min.css') }}">
-			<!-- Font Awesome -->
-			<link rel="stylesheet" href="{{ asset('backend/font-awesome/css/font-awesome.min.css') }}">
-			<!-- Theme style -->
-			<link rel="stylesheet" href="{{ asset('backend/dist/css/AdminLTE.min.css') }}">
-			<!-- Custom css -->
-			<link rel="stylesheet" href="{{ asset('backend/dist/css/custom.min.css') }}">
-
-
-			<!-- AdminLTE Skins. Choose a skin from the css/skins
-				folder instead of downloading all of them to reduce the load. -->
-			<link rel="stylesheet" href="{{ asset('backend/dist/css/skins/_all-skins.min.css') }}">
-			<!-- iCheck -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/iCheck/flat/blue.min.css') }}">
-			<!-- Morris chart -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/morris/morris.min.css') }}">
-			<!-- jvectormap -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/jvectormap/jquery-jvectormap-1.2.2.min.css') }}">
-			<!-- Daterange picker -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/daterangepicker/daterangepicker.min.css') }}">
-			<!-- Date Picker -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/datepicker/datepicker3.min.css') }}">
-
-			<!-- bootstrap wysihtml5 - text editor -->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
-
-			<!--datatable style-->
-			<link rel="stylesheet" href="{{ asset('backend/plugins/datatables/dataTables.bootstrap.min.css') }}">
-			<link rel="stylesheet" href="{{ asset('backend/plugins/datatables/jquery.dataTables.min.css') }}">
-			<link rel="stylesheet" href="{{ asset('backend/plugins/DataTables-1.10.18/css/jquery.dataTables.min.css') }}">
-			<link rel="stylesheet" href="{{ asset('backend/plugins/Responsive-2.2.2/css/responsive.dataTables.min.css') }}">
-			<!--Select2-->
-
-			<link rel="stylesheet" type="text/css" href="{{ asset('js/intl-tel-input-13.0.0/build/css/intlTelInput.min.css') }}">
-			<link href="{{ asset('backend/plugins/select2/select2.min.css') }}" rel="stylesheet" type="text/css" />
-			<link href="{{ asset('backend/css/style2.min.css') }}" rel="stylesheet" type="text/css" />
-			<link href="{{ asset('backend/css/style.min.css') }}" rel="stylesheet" type="text/css" />
-			<link href="{{ asset('css/glyphicon.min.min.css') }}" rel="stylesheet" type="text/css" />
-			@stack('css')
-		</head>
-	<body class="hold-transition skin-blue sidebar-mini">
-	<div class="wrapper">
+<head>
+	<title>Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes</title>
+	<meta charset="utf-8" />
+	<meta name="description"
+		content="The most advanced Tailwind CSS & Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. Grab your copy now and get life-time updates for free." />
+	<meta name="keywords"
+		content="tailwind, tailwindcss, metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title"
+		content="Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes" />
+	<meta property="og:url" content="https://keenthemes.com/metronic" />
+	<meta property="og:site_name" content="Metronic by Keenthemes" />
+	<link rel="canonical" href="http://preview.keenthemes.comindex.html" />
+	<link rel="shortcut icon" href="{{asset('backend/assets/media/logos/favicon.ico')}} "/>
+	<!--begin::Fonts(mandatory for all pages)-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+	<!--end::Fonts-->
+	<!--begin::Vendor Stylesheets(used for this page only)-->
+	<link href="{{ asset('backend/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet"
+		type="text/css" />
+	<link href="{{ asset('backend/assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet"
+		type="text/css" />
+	<!--end::Vendor Stylesheets-->
+	<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+	<link href="{{ asset('backend/assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+	<link href="{{asset('backend/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+	<!--end::Global Stylesheets Bundle-->
+	<script>// Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }</script>
+</head>
