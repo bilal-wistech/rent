569be82c03a9e7b09764cb97eb88f0e554794422
maryam-malik86 <maryamalik0707@gmail.com>
2024-10-21 11:26:18 +0500

    Add country and area functionality

---
+++ app/DataTables/CityDataTable.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\City;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Services\DataTable;
+
+class CityDataTAble extends DataTable
+{
+ public $countryId;
+
+    public function __construct($countryId = null)
+    {
+        $this->countryId = $countryId;
+    }
+
+    public function ajax()
+    {
+        return datatables()
+            ->eloquent($this->query())
+            ->addColumn('action', function ($city) {
+                // Add '#' in the href for all the links
+                $view = '<a href="' . route('area.show', $city->id) . '" class="btn btn-xs btn-info">
+                <i class="fa fa-home" style="color: white;"></i>
+             </a>';
+     $edit = '<a href="' . route('city.edit', $city->id) . '" class="btn btn-xs btn-primary" onclick="editCity(' . $city->id . ')"><i class="fa fa-edit"></i></a>&nbsp;';
+                $delete = '
+                <form action="' . route('city.destroy', $city->id) . '" method="POST" style="display:inline;">
+                    ' . csrf_field() . '
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button type="submit" class="btn btn-xs btn-danger">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </form>';
+
+                return $view . ' ' . $edit . ' ' . $delete;
+            })
+
+
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+    public function query()
+{
+    $query = City::query();
+ if ($this->countryId) {
+        $query->where('country_id', $this->countryId);
+    }
+ $query->orderBy('id', 'asc');
+  return $this->applyScopes($query);
+}
+
+
+public function html()
+{
+    return $this->builder()
+        ->addColumn(['data' => 'id', 'name' => 'cities.id', 'title' => 'ID', 'orderable' => true])
+        ->addColumn(['data' => 'name', 'name' => 'cities.name', 'title' => 'Name', 'orderable' => true])
+        ->addColumn(['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => false, 'searchable' => false])
+        ->parameters(dataTableOptions());
+}
+
+
+    protected function filename()
+    {
+        return 'City_' . date('YmdHis');
+    }
+}
--- app/Http/Controllers/Admin/CityController.php
+++ app/Http/Controllers/Admin/CityController.php
@@ -2,84 +2,95 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\City;
 use Illuminate\Http\Request;
+use App\DataTables\CityDataTAble;
+use App\Http\Controllers\Controller;
 
 class CityController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+      }
     public function create()
     {
-        //
+
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'country_id' => 'required',
+        ]);
+        try {
+            City::create([
+                'name' => $request->name,
+                'country_id' => $request->country_id,
+            ]);
+
+            return redirect()->back()
+            ->with('success', 'City added successfully!')
+            ->with('countryId', $request->country_id);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return redirect()->back()->with('error', 'An error occurred while adding the city.');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+     public function show($countryId)
+{
+    try {
+        $dataTable = new CityDataTAble($countryId);
+        return $dataTable->render('admin.city.view', ['countryId' => $countryId]);
+    } catch (\Exception $e) {
+        \Log::error($e->getMessage());
+        return response()->json(['error' => 'An error occurred.'], 500);
+    }
+}
+
+    public function add($countryId)
     {
-        //
+        return view('admin.city.add', compact('countryId'));
+    }
+    public function edit($countryId)
+    {      $city = City::find($countryId);
+        return view('admin.city.edit', compact('city'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+
+            $city = City::find($id);
+            $city->name = $request->name;
+            $city->save();
+            session()->flash('success', 'City updated successfully');
+            return view('admin.city.edit', [
+                'city' =>$city,
+
+            ]);
+
+    }
     public function destroy($id)
     {
-        //
+
+        $city = City::find($id);
+
+        if ($city) {
+            $city->delete();
+            return redirect()->back()->with('success', 'City deleted successfully.');
+        }
+        return redirect()->back()->with('error', 'City not found.');
     }
+
 }
