be40c9351c7ff6af8fd65187a2f7396a7c3837bd
Bilal Ahmed <bilal.ahmed@wistech.biz>
2024-10-23 17:54:59 +0500

    area functionality added

--- routes/web.php
+++ routes/web.php
@@ -111,6 +111,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['gue
     Route::get('properties', 'PropertiesController@index')->middleware(['permission:properties']);
     Route::match(array('GET', 'POST'), 'add-properties', 'PropertiesController@add')->middleware(['permission:add_properties']);
     Route::get('properties/cities-by-country/{country}', 'PropertiesController@getCitiesByCountry')->name('cities-by-country');
+    Route::get('properties/get-areas/{country}/{city}', 'PropertiesController@getAreas')->name('get-areas');
     Route::get('properties/property_list_csv', 'PropertiesController@propertyCsv');
     Route::get('properties/property_list_pdf', 'PropertiesController@propertyPdf');
 
