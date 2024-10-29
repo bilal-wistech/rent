ca8260cef6e7691e4a2dcf2c0c1f50b77375a755
RanaSikander44 <sikander@wistech.biz>
2024-10-29 16:55:56 +0500

    settings fixes

--- resources/views/admin/propertyTypes/view.blade.php
+++ resources/views/admin/propertyTypes/view.blade.php
@@ -1,36 +1,54 @@
 @extends('admin.template')
 
 @section('main')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Main content -->
-        <section class="content">
-            <div class="row">
-                <div class="col-lg-3 col-12 settings_bar_gap">
-                    @include('admin.common.settings_bar')
-                </div>
-                <div class="col-lg-9 col-12">
+<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+    <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+        <div class="d-flex flex-column flex-column-fluid">
+            <div id="kt_app_content" class="app-content flex-column-fluid">
+                <div id="kt_app_content_container" class="app-container container-fluid">
+                    <section class="content">
+                        <div class="row">
+                            <div class="col-lg-3 col-12 settings_bar_gap">
+                                @include('admin.common.settings_bar')
+                            </div>
+                            <div class="col-lg-9 col-12">
 
-                    <div class="box box_info">
-                        <div class="box-header">
-                            <h3 class="box-title">Property type Management</h3>
-                            <div class="pull-right"><a class="btn btn-success f-14" href="{{ url('admin/settings/add-property-type') }}">Add Property Type</a></div>
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="parent-table filters-parent f-14">
-                                {!! $dataTable->table(['class' => 'table table-striped dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+                                <div class="card">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h3 class="box-title">Property type Management</h3>
+                                        <div class="pull-right"><a class="btn btn-success btn-sm f-14"
+                                                href="{{ url('admin/settings/add-property-type') }}">Add Property
+                                                Type</a></div>
+                                    </div>
+                                    <!-- /.box-header -->
+                                    <div class="card-body">
+                                        <div class="parent-table filters-parent f-14">
+                                            {!! $dataTable->table(['class' => 'table table-striped dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </section>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
+</div>
 @endsection
 
+
 @push('scripts')
-    <script src="{{ asset('backend/plugins/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('backend/plugins/Responsive-2.2.2/js/dataTables.responsive.min.js') }}"></script>
+    <!-- DataTables CSS and JS -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- DataTables CSS and JS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
     {!! $dataTable->scripts() !!}
 @endpush
