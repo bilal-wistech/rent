366024940796eefc1ef1eee98112598557dfdf96
RanaSikander44 <sikander@wistech.biz>
2024-10-29 13:01:34 +0500

    reviews module

--- resources/views/admin/amenities/view.blade.php
+++ resources/views/admin/amenities/view.blade.php
@@ -2,40 +2,63 @@
 
 @section('main')
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-	<!-- Content Header (Page header) -->
-	<section class="content-header">
-		<h1>
-			Amenities
-			<small>Control panel</small>
-		</h1>
-		@include('admin.common.breadcrumb')
-	</section>
+<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+	<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+		<div class="d-flex flex-column flex-column-fluid">
+			<section class="content-header">
+				<h3 class="mb-4 ml-4">
+					Amenities
+					<small>Control panel</small>
+				</h3>
+				<div class="ml-4 mr-4">
+					@include('admin.common.breadcrumb')
+				</div>
+			</section>
 
-	<!-- Main content -->
-	<section class="content">
-		<div class="row">
-			<div class="col-xs-12">
-				<div class="box">
-					<div class="box-header">
-					<h3 class="box-title">Amenities Management</h3>
-					<div class="pull-right"><a class="btn btn-success f-14" href="{{ url('admin/add-amenities') }}">Add Amenities</a></div>
-					</div>
-					<!-- /.box-header -->
-					<div class="box-body">
-						<div class="table-responsive parent-table filters-parent f-14">
-							{!! $dataTable->table(['class' => 'table table-striped table-hover dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+			<!-- Main content -->
+			<div id="kt_app_content" class="app-content flex-column-fluid">
+				<div id="kt_app_content_container" class="app-container container-fluid">
+					<section class="content">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="card">
+									<div class="card-header d-flex justify-content-between align-items-center">
+										<h3 class="card-title">Amenities Management</h3>
+										<div>
+											<a class="btn btn-sm btn-success"
+												href="{{ url('admin/add-amenities') }}">Add Amenities</a>
+										</div>
+									</div>
+									<div class="card-body">
+										<div class="table-responsive">
+											<div class="parent-table filters-parent f-14">
+												{!! $dataTable->table(['class' => 'table table-striped table-hover dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+					</section>
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
 </div>
 @endsection
 
 @push('scripts')
-<script src="{{ asset('backend/plugins/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/Responsive-2.2.2/js/dataTables.responsive.min.js') }}"></script>
-{!! $dataTable->scripts() !!}
+	<!-- jQuery -->
+	<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+	<!-- DataTables CSS and JS -->
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+	<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+	{!! $dataTable->scripts() !!}
+
 @endpush
--- resources/views/admin/reviews/view.blade.php
+++ resources/views/admin/reviews/view.blade.php
@@ -1,103 +1,145 @@
 @extends('admin.template') 
 @section('main')
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-<!-- Content Header (Page header) -->
-<section class="content-header">
-	<h1>Reviews<small>Control panel</small></h1>
-@include('admin.common.breadcrumb')
-</section>
-<!-- Main content -->
-<section class="content">
-	<!--Filtering Box Start -->
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="box">
-				<div class="box-body">
-					<form class="form-horizontal" enctype='multipart/form-data' action="{{ url('admin/reviews') }}" method="GET" accept-charset="UTF-8">
-						{{ csrf_field() }}
-						<div class="col-md-12  d-none">
-							<input class="form-control" type="text" id="startDate"  name="from" value="{{ isset($from) ? $from : '' }}" hidden>
-							<input class="form-control" type="text" id="endDate"  name="to" value="{{ isset($to) ? $to : '' }}" hidden>
-						</div>
-
-                        <div class="row align-items-center date-parent">
-                            <div class="col-md-3 col-sm-3 col-xs-12">
-                                <label>Date Range</label>
-                                <div class="input-group col-xs-12">
-                                    <button type="button" class="form-control" id="daterange-btn">
-                                    <span class="pull-left">
-                                        <i class="fa fa-calendar"></i>  Pick a date range
-                                    </span>
-                                    <i class="fa fa-caret-down pull-right"></i>
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4 col-sm-3 col-xs-12">
-                                <label>Property</label>
-                                <select class="form-control" name="property" id="property">
-                                    <option value="">All</option>
-                                    @if (!empty($property))
-                                        @foreach ($property as $properties)
-                                        <option value="{{ $properties->id }}" {{ $properties->id == $allproperties ? ' selected = "selected"' : '' }}>{{ $properties->name }}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
-                            </div>
-
-                            <div class="col-md-3 col-sm-2 col-xs-12">
-                                <label>Reviewer</label>
-                                <select class="form-control" name="reviewer" id="reviewer">
-                                    <option value="" >All</option>
-                                    <option value="guest" {{ $allreviewer == "guest" ? ' selected = "selected"' : '' }}>Guest</option>
-                                    <option value="host" {{ $allreviewer == "host" ? ' selected = "selected"' : '' }}>Host</option>
-                                </select>
-                            </div>
-
-                            <div class="col-md-1 col-sm-2 col-xs-4 d-flex gap-2 mt-4">
-                                <button type="submit" name="btn" class="btn btn-primary btn-flat f-14 rounded">Filter</button>
-                                <button type="button" name="reset_btn" id="reset_btn" class="btn btn-primary btn-flat f-14 rounded">Reset</button>
-                            </div>
-                        </div>
-					</form>
+<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+	<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+		<div class="d-flex flex-column flex-column-fluid">
+			<class="content-header">
+				<h3 class="mb-4 ml-4">
+					Reviews
+					<small>Control panel</small>
+				</h3>
+				<div class="ml-4 mr-4">
+					@include('admin.common.breadcrumb')
 				</div>
-			</div>
-		</div>
-	</div>
-	<!--Filtering Box End -->
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="box">
-				<div class="box-header">
-					<h3 class="box-title">Reviews Management</h3>
-				</div>
-				<!-- /.box-header -->
-				<div class="box-body">
-					<div class="table-responsive parent-table f-14">
-						{!! $dataTable->table(['class' => 'table table-striped table-hover dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+
+				<div id="kt_app_content" class="app-content flex-column-fluid">
+					<div id="kt_app_content_container" class="app-container container-fluid">
+						<section class="content">
+							<!-- Filtering Card Start -->
+							<div class="card mb-4">
+								<div class="card-header">
+									<h5 class="card-title">Filter Reviews</h5>
+								</div>
+								<div class="card-body">
+									<form class="form-horizontal" enctype='multipart/form-data' action="{{ url('admin/reviews') }}" method="GET" accept-charset="UTF-8">
+										{{ csrf_field() }}
+										<div class="col-md-12 d-none">
+											<input class="form-control" type="text" id="startDate" name="from" value="{{ isset($from) ? $from : '' }}" hidden>
+											<input class="form-control" type="text" id="endDate" name="to" value="{{ isset($to) ? $to : '' }}" hidden>
+										</div>
+
+										<div class="row align-items-center date-parent">
+											<div class="col-md-3 col-sm-3 col-xs-12">
+												<label>Date Range</label>
+												<div class="input-group">
+													<input type="text" class="form-control" id="dateRange" name="date_range" placeholder="Select date range" value="{{ isset($date_range) ? $date_range : '' }}">
+													<div class="input-group-append">
+														<span class="input-group-text"><i class="fa fa-calendar"></i></span>
+													</div>
+												</div>
+												<input class="form-control d-none" type="text" id="startDate" name="from" value="{{ isset($from) ? $from : '' }}">
+												<input class="form-control d-none" type="text" id="endDate" name="to" value="{{ isset($to) ? $to : '' }}">
+											</div>
+
+											<div class="col-md-4 col-sm-3 col-xs-12">
+												<label>Property</label>
+												<select class="form-control" name="property" id="property">
+													<option value="">All</option>
+													@if (!empty($property))
+														@foreach ($property as $properties)
+															<option value="{{ $properties->id }}" {{ $properties->id == $allproperties ? ' selected' : '' }}>
+																{{ $properties->name }}</option>
+														@endforeach
+													@endif
+												</select>
+											</div>
+
+											<div class="col-md-3 col-sm-2 col-xs-12">
+												<label>Reviewer</label>
+												<select class="form-control" name="reviewer" id="reviewer">
+													<option value="">All</option>
+													<option value="guest" {{ $allreviewer == "guest" ? ' selected' : '' }}>Guest</option>
+													<option value="host" {{ $allreviewer == "host" ? ' selected' : '' }}>Host</option>
+												</select>
+											</div>
+
+											<div class="col-md-1 col-sm-2 col-xs-4 d-flex gap-2 mt-4">
+												<button type="submit" name="btn" class="btn btn-primary btn-flat f-14 rounded">Filter</button>
+												<button type="button" name="reset_btn" id="reset_btn" class="btn btn-primary btn-flat f-14 rounded">Reset</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							<!-- Filtering Card End -->
+
+							<!-- Table Card Start -->
+							<div class="card">
+								<div class="card-header">
+									<h5 class="card-title">Reviews Management</h5>
+								</div>
+								<div class="card-body">
+									<div class="table-responsive parent-table f-14">
+										{!! $dataTable->table(['class' => 'table table-striped table-hover dt-responsive', 'width' => '100%', 'cellspacing' => '0']) !!}
+									</div>
+								</div>
+							</div>
+							<!-- Table Card End -->
+						</section>
 					</div>
 				</div>
-			</div>
 		</div>
 	</div>
-</section>
 </div>
 @endsection
-@push('scripts')
-<script src="{{ asset('backend/plugins/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('backend/plugins/Responsive-2.2.2/js/dataTables.responsive.min.js') }}"></script>
-{!! $dataTable->scripts() !!} 
-@endpush 
+
 @section('validate_script')
-	<script type="text/javascript">
-		'use strict'
-		var from      = $('#startDate').val();
-		var to        = $('#endDate').val();
-		var sessionDate  = '{{strtoupper(Session::get('date_format_type'))}}';
-		var page = 'review'
-		
-	</script>
-	<script src="{{ asset('backend/js/reset-btn.min.js') }}"></script>
-	<script src="{{ asset('backend/js/admin-date-range-picker.min.js') }}"></script>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+	crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+<!-- DataTables CSS and JS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+	$(document).ready(function () {
+		// Initialize Flatpickr for date range
+		flatpickr("#dateRange", {
+			mode: "range",
+			dateFormat: "m-d-Y", // Change to m-d-Y to match your database format
+			onChange: function (selectedDates) {
+				if (selectedDates.length === 2) {
+					// Format the dates to m-d-Y
+					$('#startDate').val(flatpickr.formatDate(selectedDates[0], "m-d-Y"));
+					$('#endDate').val(flatpickr.formatDate(selectedDates[1], "m-d-Y"));
+				}
+			}
+		});
+
+		// Handle reset button
+		$('#reset_btn').on('click', function () {
+			$('#startDate, #endDate').val(''); // Clear date fields
+			$('#status, #space_type').val(''); // Reset other fields
+			$('#dateRange').val(''); // Reset date range field
+			window.location.href = '{{ url("admin/reviews") }}'; // Redirect to reset filters
+		});
+	});
+</script>
+
+{!! $dataTable->scripts() !!}
+
+<script>
+	var sessionDate = '{{ strtoupper(Session::get('date_format_type')) }}';
+	var user_id = '{{ $user->id ?? '' }}';
+	var page = "review";
+</script>
+
+<script src="{{ asset('backend/js/reset-btn.min.js') }}"></script>
+<script src="{{ asset('backend/js/admin-date-range-picker.min.js') }}"></script>
 @endsection
