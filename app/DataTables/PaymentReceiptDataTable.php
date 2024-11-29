8f81cf5dbd07fc14c247e988ed7b53e96cbbd9af
Bilal Ahmed <bilal.ahmed@wistech.biz>
2024-11-29 11:23:52 +0500

    status field added in the payment receipts table

--- app/DataTables/PaymentReceiptDataTable.php
+++ app/DataTables/PaymentReceiptDataTable.php
@@ -8,6 +8,7 @@ use App\Models\Invoice;
 use App\Models\Bookings;
 use App\Models\Currency;
 use App\Http\Helpers\Common;
+use App\Models\PropertyDates;
 use App\Models\PaymentReceipt;
 use Yajra\DataTables\Services\DataTable;
 
@@ -19,6 +20,7 @@ class PaymentReceiptDataTable extends DataTable
         $payment_receipts = $this->query();
         return datatables()
             ->of($payment_receipts)
+            ->addIndexColumn()
             ->addColumn('booking_id', function ($payment_receipts) {
                 return $payment_receipts->booking_id;
             })
@@ -36,8 +38,28 @@ class PaymentReceiptDataTable extends DataTable
                 return $currencyDefault->code.' '.Common::convert_currency(
                     '',
                     $currencyDefault->code,
-                    $payment_receipt->amount
+                    $payment_receipt->amount ?? 0
                 );
+            })
+            ->addColumn('total_amount', function ($payment_receipt) use ($currencyDefault) {
+
+                return $currencyDefault->code.' '.Common::convert_currency(
+                    '',
+                    $currencyDefault->code,
+                    $payment_receipt->booking->total ?? 0
+                );
+            })
+            ->addColumn('status', function ($payment_receipts) {
+                // Fetch the status for the specific date range
+                $property_id = $payment_receipts->booking->property_id;
+                $start_date = $payment_receipts->booking->start_date;
+                $end_date = $payment_receipts->booking->end_date;
+
+                $property_dates = PropertyDates::where('property_id', $property_id)
+                    ->whereBetween('date', [$start_date, $end_date])
+                    ->first(); // Get the first matching record
+
+                return $property_dates ? $property_dates->status : 'N/A';
             })
             ->addColumn('created_at', function ($payment_receipts) {
                 return dateFormat($payment_receipts->created_at);
@@ -47,7 +69,7 @@ class PaymentReceiptDataTable extends DataTable
             //         '<a href="' . url('admin/payment-receipts/edit/' . $payment_receipts->id) . '" class="btn btn-xs btn-primary" title="Edit"><i class="fa fa-edit"></i></a>&nbsp;';
 
             // })
-            ->rawColumns(['booking_id', 'property_id', 'user_id', 'payment_date', 'amount'])
+            ->rawColumns(['DT_RowIndex','booking_id', 'property_id', 'user_id', 'payment_date', 'amount','total_amount','status','created'])
             ->make(true);
     }
 
@@ -70,14 +92,16 @@ class PaymentReceiptDataTable extends DataTable
     public function html()
     {
         return $this->builder()
-            ->addColumn(['data' => 'booking_id', 'name' => 'booking_id', 'title' => 'Booking ID'])
-            ->addColumn(['data' => 'property_id', 'name' => 'property_id', 'title' => 'Property Name'])
-            ->addColumn(['data' => 'user_id', 'name' => 'user_id', 'title' => 'Customer Name'])
-            ->addColumn(['data' => 'paid_through', 'name' => 'paid_through', 'title' => 'Paid Through'])
-            ->addColumn(['data' => 'payment_date', 'name' => 'payment_date', 'title' => 'Payment Date'])
-            ->addColumn(['data' => 'amount', 'name' => 'amount', 'title' => 'Amount Paid'])
-            // ->addColumn(['data' => 'status', 'name' => 'status', 'title' => 'Payment Status'])
-            ->addColumn(['data' => 'created_at', 'name' => 'created_at', 'title' => 'Created At'])
+        ->addColumn(['data' => 'DT_RowIndex', 'name' => 'DT_RowIndex', 'title' => '#', 'orderable' => false, 'searchable' => false])
+            ->addColumn(['data' => 'booking_id', 'name' => 'booking_id', 'title' => 'Booking ID', 'orderable' => true, 'searchable' => false])
+            ->addColumn(['data' => 'property_id', 'name' => 'property_id', 'title' => 'Property', 'orderable' => true, 'searchable' => true])
+            ->addColumn(['data' => 'user_id', 'name' => 'user_id', 'title' => 'Tenant', 'orderable' => true, 'searchable' => true])
+            ->addColumn(['data' => 'paid_through', 'name' => 'paid_through', 'title' => 'Paid Through', 'orderable' => true, 'searchable' => true])
+            ->addColumn(['data' => 'payment_date', 'name' => 'payment_date', 'title' => 'Payment Date', 'orderable' => true, 'searchable' => true])
+            ->addColumn(['data' => 'amount', 'name' => 'amount', 'title' => 'Amount Paid', 'orderable' => true, 'searchable' => false])
+            ->addColumn(['data' => 'total_amount', 'name' => 'total_amount', 'title' => 'Total Amount', 'orderable' => true, 'searchable' => false])
+            ->addColumn(['data' => 'status', 'name' => 'status', 'title' => 'Payment Status', 'orderable' => true, 'searchable' => true])
+            ->addColumn(['data' => 'created_at', 'name' => 'created_at', 'title' => 'Created At', 'orderable' => true, 'searchable' => true])
             // ->addColumn(['data' => 'action', 'name' => 'action', 'title' => 'Action', 'orderable' => true, 'searchable' => false])
             ->parameters(dataTableOptions());
     }
