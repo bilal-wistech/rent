212e458869cacdd23bff09115a5d6dfb8147278d
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-01-18 18:53:11 +0500

    fixes

--- app/Http/Controllers/Admin/DocumentController.php
+++ app/Http/Controllers/Admin/DocumentController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Document;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class DocumentController extends Controller
