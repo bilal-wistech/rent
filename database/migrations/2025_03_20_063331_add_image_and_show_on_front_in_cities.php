79fa3cda88aa63c57b597ee7c4977d967a9a4341
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-03-20 11:36:02 +0500

    migrations for cities and areas added

---
+++ database/migrations/2025_03_20_063331_add_image_and_show_on_front_in_cities.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->string('image')->nullable()->after('name');
+            $table->tinyInteger('show_on_front')->default(1)->after('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+};
