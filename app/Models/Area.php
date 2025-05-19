49439cb5954be83b7fe439c898fc7c0123e73d01
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-05-19 17:47:43 +0500

    images issue fixedfor areas

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     use HasFactory;
+    protected $appends = ['image_url'];
     protected $fillable = [
         'name',
         'image',
@@ -15,4 +16,14 @@ class Area extends Model
         'city_id',
         'country_id',
     ];
+    public function getImageUrlAttribute()
+    {
+        $image = $this->attributes['image'] ?? null;
+
+        if (empty($image)) {
+            return asset('images/default-image.png');
+        }
+
+        return asset('front/images/front-areas/' . $image);
+    }
 }
