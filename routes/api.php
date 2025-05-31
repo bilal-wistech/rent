b7c17bb50e0f6713f63c04961b812b7fe5354583
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-05-31 14:26:56 +0500

    fixed the api detail property route

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::get('vacant-properties', [HomeController::class, 'vacantProperties']);
 Route::get('properties', [PropertyController::class, 'searchProperties']);
 Route::get('properties/{slug}', [PropertyController::class, 'show']);
 Route::get('locations', [PropertyController::class, 'getLocations']);
-Route::get('get-bookings', [BookingController::class, 'getBookings']);
+Route::post('get-bookings', [BookingController::class, 'getBookings']);
 
 Route::get('sanctum/csrf-cookie', [\Laravel\Sanctum\Http\Controllers\CsrfCookieController::class, 'show']);
 Route::post('register', [AuthController::class, 'register']);
