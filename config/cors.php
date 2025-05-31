4d682e78d7f8e2009cd9b71e1310d438cb6cec30
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-05-31 16:18:16 +0500

    changes in cors file

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','http://localhost:4173','https://wisdomskt-dev-com.stackstaging.com/ahomes-backend'],
+    'allowed_origins' => ['http://localhost:5173','http://localhost:4173','https://wisdomskt-dev-com.stackstaging.com/ahomes/backend'],
 
     'allowed_origins_patterns' => [],
 
