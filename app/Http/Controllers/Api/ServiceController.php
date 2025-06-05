2ae10cf0384e479082b8a88d68a10664ebe23cf1
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-06-05 17:45:30 +0500

    fixes

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\ServiceStoreRequest;
 use App\Models\Service;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\ServiceStoreRequest;
 
 class ServiceController extends Controller
 {
