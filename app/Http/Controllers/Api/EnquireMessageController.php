ab3a0a0389a0cf01c0e2a97dea3b9b16e5dac8cb
Bilal Ahmed <bilal.ahmed@wistech.biz>
2025-06-10 17:28:14 +0500

    namespace fixes in request

--- app/Http/Controllers/Api/EnquireMessageController.php
+++ app/Http/Controllers/Api/EnquireMessageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\EnquiryMessage;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreEnquireMessageRequest;
+use App\Http\Requests\Api\StoreEnquireMessageRequest;
 
 class EnquireMessageController extends Controller
 {
